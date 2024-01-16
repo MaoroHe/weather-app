10f4be7347b2f153584ce863209b8837943ef739
MaoroHe <maorohecq1409@gmail.com>
2024-01-16 08:57:52 +0100

    fix

--- assets/js/weather/weather.js
+++ assets/js/weather/weather.js
@@ -17,8 +17,8 @@ let infoWrite = (info) => {
     let skyState = info.list[0].weather[0].main;
     let descT = info.list[0].weather[0].description;
 
-    let mat_img = `/assets/img/${info.list[0].weather[0].main}.png`;
-    let mid_img = `/assets/img/${info.list[1].weather[0].main}.png`;
+    let mat_img = `./assets/img/${info.list[0].weather[0].main}.png`;
+    let mid_img = `./assets/img/${info.list[1].weather[0].main}.png`;
     let soi_img = `/assets/img/${info.list[2].weather[0].main}.png`;
 
     let mat_temp = info.list[0].main.temp - 273.15;
